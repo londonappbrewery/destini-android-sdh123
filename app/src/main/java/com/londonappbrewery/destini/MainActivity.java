f7f9f061d202bfd19704ecbc3fd80a596ca385ae
Simon Holford <simon.holford@gmail.com>
2017-11-22 12:10:05 +0000

    Remove log import

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,7 +2,6 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
