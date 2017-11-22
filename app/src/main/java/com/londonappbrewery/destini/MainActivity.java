ea2857f61b435aecfd2fa92389b27a9163f30629
Simon Holford <simon.holford@gmail.com>
2017-11-22 12:07:06 +0000

    Project complete!

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,29 +2,86 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO: Steps 4 & 8 - Declare member variables here:
-
+    private TextView mStory;
+    private Button mTopButton;
+    private Button mBottomButton;
+    private int mStoryIndex;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mStory = (TextView) findViewById(R.id.storyTextView);
+        mTopButton = (Button) findViewById(R.id.buttonTop);
+        mBottomButton = (Button) findViewById(R.id.buttonBottom);
 
-        // TODO: Step 5 - Wire up the 3 views from the layout to the member variables:
+        mStoryIndex = 1;
 
+        mTopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (mStoryIndex == 1 || mStoryIndex == 2) {
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the top button:
+                    // move on to next story, T3_Story
+                    mStoryIndex = 3;
 
+                    mStory.setText(R.string.T3_Story);
+                    mTopButton.setText(R.string.T3_Ans1);
+                    mBottomButton.setText(R.string.T3_Ans2);
 
+                } else if (mStoryIndex == 3) {
 
+                    // finish with T6_End
+                    mStoryIndex = 6;
 
-        // TODO: Steps 6, 7, & 9 - Set a listener on the bottom button:
+                    mStory.setText(R.string.T6_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                }
+            }
+        });
 
+        mBottomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (mStoryIndex == 1) {
+
+                    // move on to next story, T2_Story
+                    mStoryIndex = 2;
+
+                    mStory.setText(R.string.T2_Story);
+                    mTopButton.setText(R.string.T2_Ans1);
+                    mBottomButton.setText(R.string.T2_Ans2);
+
+                } else if (mStoryIndex == 2) {
+
+                    // finish with T4_End
+                    mStoryIndex = 4;
+
+                    mStory.setText(R.string.T4_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+
+                } else if (mStoryIndex == 3) {
+
+                    // finish with T5_End
+                    mStoryIndex = 5;
+
+                    mStory.setText(R.string.T5_End);
+                    mTopButton.setVisibility(View.GONE);
+                    mBottomButton.setVisibility(View.GONE);
+                }
+            }
+        });
     }
 }
